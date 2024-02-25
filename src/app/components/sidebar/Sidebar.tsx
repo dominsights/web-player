bd1606cfabf5a1de8b6450fe395f3af592c0ac33
Domício Medeiros <domicio.medeiros@proton.me>
2024-02-25 18:13:28 -0300

    add link to playlists route

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {SidebarViewModel} from "@/app/components/sidebar/SidebarViewModel";
+import Link from "next/link";
 
 function Sidebar(props: { viewModel: SidebarViewModel}) {
     return (
         <div>
             Sidebar
             <ul>
-                {props.viewModel.playlists.map(p => (<li key={p.id}>{p.name}</li>))}
+                {props.viewModel.playlists.map(p =>
+                    (
+                        <li key={p.id}>
+                            <Link href={`/playlists/${p.id}`}>{p.name}</Link>
+                        </li>
+                    ))}
             </ul>
         </div>
     );

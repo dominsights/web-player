9be5370f215fa92cad1c679eb6113a96dc3a47bd
Domício Medeiros <domicio.medeiros@proton.me>
2024-03-02 21:50:56 -0300

    ...

--- test/app/playlist/[id]/PlaylistViewModel.test.ts
+++ test/app/playlist/[id]/PlaylistViewModel.test.ts
@@ -9,33 +9,33 @@ describe('PlaylistViewModel', () => {
     const track1 = new Track("7empest", "TOOL", "Fear Inoculum", new Date(2024, 3, 2), 60 * 3);
     const track2 = new Track("Without Walls", "IQ", "The Road of Bones", new Date(2024, 3, 2), 60 * 5);
 
-    let viewModel: PlaylistViewModel; 
+    let playlistVM: PlaylistViewModel; 
 
     beforeEach(() => {
-        viewModel = new PlaylistViewModel('playlist title', 'playlist description', [track1, track2], player);
+        playlistVM = new PlaylistViewModel('playlist title', 'playlist description', [track1, track2], player);
     })
 
     it('should remove track from playlist', () => {
-        viewModel.remove(track1);
-        expect(viewModel.tracks).toEqual([track2]);
+        playlistVM.remove(track1);
+        expect(playlistVM.tracks).toEqual([track2]);
     })
 
     it('should start playing the first song on playlist when clicking play playlist', () => {
-        viewModel.playAll();
+        playlistVM.playAll();
         expect(player.currentTrack?.title).toBe(track1.title);
     });
 
     it('should play selected track', () => {
-        viewModel.play(track1);
+        playlistVM.play(track1);
     });
 
     it('should move track to new position', () => {
-        viewModel.move(track1, 2);
+        playlistVM.move(track1, 2);
     });
 
     it('should edit playlist title and description', () => {
-        viewModel.edit('new title', 'new description');
-        expect(viewModel.title).toBe('new title');
-        expect(viewModel.description).toBe('new description');
+        playlistVM.edit('new title', 'new description');
+        expect(playlistVM.title).toBe('new title');
+        expect(playlistVM.description).toBe('new description');
     });
 });

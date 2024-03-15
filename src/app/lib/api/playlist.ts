e97784a66ce8d2b45990ca699d93b2a26c1777a8
Domício Medeiros <domicio.medeiros@proton.me>
2024-03-15 17:46:24 -0300

    fix tests, extract playlist methods into playlist class

--- src/app/lib/api/playlist.ts
+++ src/app/lib/api/playlist.ts
@@ -12,4 +12,24 @@ export class Playlist {
         this.tracks = tracks;
         this.description = description;
     }
+
+    removeTrack(track: Track): Playlist {
+        const updatedTracks = this.tracks.filter(t => t !== track);
+        return new Playlist(this.id, this.title, this.description, updatedTracks);
+    }
+
+    moveTrack(track: Track, newPosition: number): Playlist {
+        const index = this.tracks.indexOf(track);
+        if (index === -1) {
+            return this;
+        }
+        const updatedTracks = [...this.tracks];
+        const removed = updatedTracks.splice(index, 1);
+        updatedTracks.splice(newPosition - 1, 0, ...removed);
+        return new Playlist(this.id, this.title, this.description, updatedTracks);
+    }
+
+    editPlaylist(newTitle: string, newDescription: string): Playlist {
+        return new Playlist(this.id, newTitle, newDescription, this.tracks);
+    }
 }

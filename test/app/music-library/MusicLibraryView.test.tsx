51dd000d559a4d93de37e73a8ea80b2c2364b498
Domício Medeiros <domicio.medeiros@proton.me>
2024-03-23 16:24:19 -0300

    fix test file

---
+++ test/app/music-library/MusicLibraryView.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react'
+import MusicLibraryView from "@/app/music-library/MusicLibraryView";
+
+describe('MusicLibraryView', () => {
+    it('should', () => {
+        render(<MusicLibraryView />);
+    })
+})

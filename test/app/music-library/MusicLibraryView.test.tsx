d131c771f6b35b8dbdea7332faf896cb4a2193f6
Domício Medeiros <domicio.medeiros@proton.me>
2024-03-24 18:28:37 -0300

    fix render test

--- test/app/music-library/MusicLibraryView.test.tsx
+++ test/app/music-library/MusicLibraryView.test.tsx
@@ -1,8 +1,13 @@
 import {render} from '@testing-library/react'
 import MusicLibraryView from "@/app/music-library/MusicLibraryView";
+import { PlayQueueProvider } from '@/app/contexts/PlayQueueContext';
 
 describe('MusicLibraryView', () => {
-    it('should', () => {
-        render(<MusicLibraryView />);
+    it('should render', () => {
+        render(
+            <PlayQueueProvider>
+                <MusicLibraryView />
+            </PlayQueueProvider>
+        );
     })
 })

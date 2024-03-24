a1b862d4e8de9cb48388e60f6305563d7672ad32
Domício Medeiros <domicio.medeiros@proton.me>
2024-03-24 16:45:55 -0300

    fix jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     '^@/app/(.*)$': '<rootDir>/src/app/$1',
   }
 };
 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
